80a01488d5699265a8463885c5930f9d31b4ab58
Sammienator <sammugo18@gmail.com>
2023-02-02 21:36:33 +0300

    minor changes

--- src/components/Navb.jsx
+++ src/components/Navb.jsx
@@ -1,6 +1,6 @@
 import React  from 'react'
 // import { AiOutlineClose,AiOutlineMenu } from 'react-icons/ai'
-import  logo from '../assets/logo.jpeg'
+
 
 
 
@@ -16,16 +16,10 @@ const Navbar = () => {
   return (
 
     
-    <div  id='back' className= 'flex  justify-between items-center   h-24 max-w-[1240px] mx-auto px-4 text-black  ' >
+    <div  id='back' className= 'flex  justify-end  items-center   h-24 max-w-[1240px] mx-auto px-4 text-black  ' >
+         
         
-
-
-        <img className='w-20  mt-10 shadow-xl '  src={logo} alt=" my pic" />
-        
-
-        
-        
-      <ul className=' flex flex-row  mt-10 text-xl  hover:cursor-pointer text-[#E63E00]  font-bold underline'>
+      <ul className=' flex flex-row  mt-10 text-xl items-center   hover:cursor-pointer text-[#E63E00]  font-bold underline'>
         <li className='p-4 hover:scale-105  duration-500 ease-in-out s '><a href="#hero">Home.  </a> </li>
         {/* <li className=' p-4 hover:scale-105 duration-500 ease-in-out shadow '> <a href="#about"> About.  </a>  </li> */}
         <li className='p-4 hover:scale-105 duration-500 ease-in-out '><a href="#product">Products.  </a>  </li>
