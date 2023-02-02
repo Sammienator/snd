41c2c356f46158dd2822e2605b9f2ed4a35852a3
Sammienator <sammugo18@gmail.com>
2023-02-02 23:22:59 +0300

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import pic2 from '../assets/pic2.jpg'
+import about from '../assets/about.jpg'
 
 const Analytics = () => {
   return (
     <div id='about' className='w-full bg-white py-8 px-4 ' >
        <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <img className='w-[500px] mx-auto my-4 h-[300px] rounded-lg hover:scale-105 duration-500 ease-in-out'  src={pic2} alt="/" />
+        <img className='w-[500px] mx-auto my-4 h-[300px] rounded-lg hover:scale-105 duration-500 ease-in-out'  src={about} alt="/" />
         <div className='flex flex-col justify-center'>
-          <p className='text-[white] font-bold'> WHAT DO WE OFFER</p>
+          <p className='text-[white] font-bold text-5xl'> WHAT DO WE OFFER?</p>
           <h1 className='md:text-4xl sm:text-2xl font-bold py-2'> A new lease at life: for you and your skin. </h1>
           <p>Caring for your skin is our commitment, because our products are tailor made in understanding of what your skin needs. Give your body the pampering that it deserves, a soap worthy of your skin with the best organic soaps tailor made for you. Show off that beautiful smooth skin, as you feel renewed and confident with S&D NATURALS products!</p>
          
 
-          <a href="#equipments" className='bg-black w-[200px] rounded-md  my-4 mx-auto  py-6 text-[#E63E00] text-center font-bold hover:scale-105 duration-500 ease-in-out'>
+          <a href="#equipments" className='bg-black w-[200px] rounded-md  my-4 mx-auto  py-6 text-[white] text-center font-bold hover:scale-105 duration-500 ease-in-out'>
           <button > Find out More.</button>
           </a>
 
